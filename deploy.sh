716674eb16ae6af99bca5b0760d4cf8922614df3
aggarwaldevops <118600128+aggarwaldevops@users.noreply.github.com>
2024-10-28 08:40:29 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,1 @@
 #!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-# FTP server details
-FTP_HOST="$1"
-FTP_USERNAME="$2"
-FTP_PASSWORD="$3"
-LOCAL_DIR="$4"
-REMOTE_DIR="$5"
-
-# Print debug information
-echo "FTP_HOST: $FTP_HOST"
-echo "FTP_USERNAME: $FTP_USERNAME"
-echo "LOCAL_DIR: $LOCAL_DIR"
-echo "REMOTE_DIR: $REMOTE_DIR"
