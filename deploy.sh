284b619246be8fef2019e1978908487cd86ee0f0
aggarwaldevops <118600128+aggarwaldevops@users.noreply.github.com>
2024-10-28 09:48:52 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Use lftp to mirror the local directory to the remote directory, skipping SSL verification
-lftp -u "betatestrepo","tP-Db8a&Mj1l27@QXf" -e "set ssl:verify-certificate no; ftp:passive-mode no; mirror -R ./ /public_html; bye" ftp.betatestrepo.netvision.com.au
+lftp -u "betatestrepo","tP-Db8a&Mj1l27@QXf" -e "set ssl:verify-certificate no; mirror -R ./ /public_html; bye" ftp.betatestrepo.netvision.com.au
